65dacec62bbe42e920bf51d4952d83d6b0335b25
Gytis <g.babaitis1@gmail.com>
2024-01-05 21:11:23 +0200

    init final course project repository

---
+++ src/main/java/com/Babaitis/Project/ProblemStore/ProblemStoreApplication.java
@@ -0,0 +1,13 @@
+package com.Babaitis.Project.ProblemStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Babaitis/Project/ProblemStore/ProblemStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Babaitis.Project.ProblemStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
