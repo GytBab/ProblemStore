a35bd724da3111641f206cd096ddda46bb3a9529
Gytis <g.babaitis1@gmail.com>
2024-01-12 20:28:38 +0200

    [01-12] refactor to have separate http endpoints in a separate file and deleted TestController

---
+++ src/main/java/com/Babaitis/Project/ProblemStore/HttpEndPoints.java
@@ -0,0 +1,7 @@
+package com.Babaitis.Project.ProblemStore;
+
+public class HttpEndPoints {
+    public static final String PRODUCTS = "/problems";
+    public static final String PRODUCTS_CREATE = PRODUCTS + "/create";
+
+}
--- src/main/java/com/Babaitis/Project/ProblemStore/problem/ProblemController.java
+++ src/main/java/com/Babaitis/Project/ProblemStore/problem/ProblemController.java
@@ -1,5 +1,6 @@
 package com.Babaitis.Project.ProblemStore.problem;
 
+import com.Babaitis.Project.ProblemStore.HttpEndPoints;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,19 +17,18 @@ public class ProblemController {
         this.problemService = problemService;
     }
 
-    @GetMapping("/problems/create")
+    @GetMapping(HttpEndPoints.PRODUCTS_CREATE)
     public String problem(Model model) {
         model.addAttribute("problem", new Problem());
         return "problem";
     }
 
-    @PostMapping("/problems/create")
+    @PostMapping(HttpEndPoints.PRODUCTS_CREATE)
     public String createNewProduct(Problem problem) {
-
         problemService.saveProblem(problem);
         System.out.println("currently in the database");
         problemService.getAllProblems().forEach(System.out::println);
-        return "welcome/welcome";
+        return "/welcome/welcome";
     }
 
 
