ce666a91b0ed52a8f3e9d5c0e09f05f9792dfe52
Gytis <g.babaitis1@gmail.com>
2024-02-01 20:14:34 +0200

    [02-01] updated problem create/update form using bootstrap. Added validation for solution and return of error with thymeleaf and css file (with bug of no dropdowns when error occurs)

--- src/main/java/com/Babaitis/Project/ProblemStore/problem/dto/ProblemDto.java
+++ src/main/java/com/Babaitis/Project/ProblemStore/problem/dto/ProblemDto.java
@@ -3,7 +3,7 @@ package com.Babaitis.Project.ProblemStore.problem.dto;
 import com.Babaitis.Project.ProblemStore.cause.Cause;
 import com.Babaitis.Project.ProblemStore.effect.Effect;
 import com.Babaitis.Project.ProblemStore.laser.Laser;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,7 +18,7 @@ public class ProblemDto {
     private Laser laser;
     private Effect effect;
     private Cause cause;
-    @NotNull
+    @NotBlank (message = "{problemDto.solution.notBlank}")
     private String solution;
     private String partNo;
     private String entryDate;
